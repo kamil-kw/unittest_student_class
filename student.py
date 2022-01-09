014aea9ae640d8d229c12a222bde09e5f837aaf9
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-01-09 19:54:39 +0000

    Chalange

--- student.py
+++ student.py
@@ -1,16 +1,28 @@
 from datetime import date, timedelta
 
+
 class Student:
-""" A Student class as a basis for method testing """
+    """ A Student class as a basis for method testing """
 
     def __init__(self, first_name, last_name):
         self._first_name = first_name
         self._last_name = last_name
-        self._start_date = date.today()
+        self.start_date = date.today()
         self.end_date = date.today() + timedelta(days=365)
-        self.noughty_list = False
-
+        self.naughty_list = False
 
     @property
     def full_name(self):
-        return f"{self._first_name}{self._last_name}"
+        return f"{self._first_name} {self._last_name}"
+
+    @property
+    def email(self):
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"\
+
+
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
+
+
+    def alert_santa(self):
+        self.naughty_list = True
